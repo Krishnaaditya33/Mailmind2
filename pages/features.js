ceb16fe7a80e965422d058cd1491ba43cedfa664
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-04-07 03:40:31 +0530

    Create features.js

---
+++ pages/features.js
@@ -0,0 +1,11 @@
+import NavBar from "../components/NavBar"; // Correct the path based on your folder structure
+
+export default function Features() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Features Page</h1>
+      <p>List of features...</p>
+    </div>
+  );
+}
