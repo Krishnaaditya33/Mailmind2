12ae0f59f47b90a9997a7fbf710a5c73463b6629
Krishnaaditya33 <krishnaaditya743@gmail.com>
2025-04-07 08:09:54 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true, // Optional in Next.js 15, can remove
   output: 'export',
-  distDir: 'build', // this is okay
   trailingSlash: true,
   rewrites: async () => [],
   redirects: async () => [],
